c09b58d7407f012cb1e187acdd0573ee37d54943
George Yu <yugeorge@uw.edu>
2014-10-13 12:12:15 -0700

    V2

---
+++ js/app.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var name = 'George';
+var fruits = ['apple', 'pear', 'kiwi', 'banana'];
+
+
+function sayHello() {
+    alert('Hello ' + name);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var idx;
+    var fruitListElem = document.getElementById('fruit-list');
+    var fruitLi;
+    var animalSound = document.getElementById('animal-sound');
+
+    var msgElem = document.getElementById('message');
+    msgElem.innerHTML = 'Hello ' + name;
+
+    for (idx = 0; idx < fruits.length; ++idx) {
+        console.log(fruits[idx]);
+
+        fruitLi = document.createElement('li');
+        fruitLi.innerHTML = fruits [idx];
+        fruitListElem.appendChild(fruitLi);
+    }
+
+    var animalPic = document.getElementById('animal-picture');
+    animalPic.addEventListener('click', function() {
+        msgElem.innerHTML = 'Bark!';
+        animalSound.play();
+    });
+
+    animalSound.addEventListener('ended', function() {
+        msgElem.innerHTML = '&nbsp';
+    });
+
+    window.setInterval(function() {
+        document.getElementById('clock').innerHTML = new Date().toLocaleString();
+    }, 1000);
+});
